9c6fb82628fe717c91419754d81e2424fa8e880c
NabilaEky <149370826+NabilaEky@users.noreply.github.com>
2026-03-23 20:46:09 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -82,8 +82,7 @@ for col in binary_cols:
     df_vis[col] = df_vis[col].map({0: "No", 1: "Yes"})
 
 # ROW 1
-col1 = st.container()
-col2 = st.container()
+col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("### 1. Distribusi Status")
@@ -100,8 +99,7 @@ with col2:
     st.pyplot(fig2, use_container_width=True)
 
 # ROW 2
-col3 = st.container()
-col4 = st.container()
+col3, col4 = st.columns(2)
 
 with col3:
     st.markdown("### 3. Pengaruh beasiswa terhadap siswa dropout")
@@ -120,8 +118,7 @@ with col4:
 st.markdown("---")
 
 # ROW 3
-col5 = st.container()
-col6 = st.container()
+col5, col6 = st.columns(2)
 
 with col5:
     st.markdown("### 5. Pengaruh nilai semester 2 terhadap status siswa")
